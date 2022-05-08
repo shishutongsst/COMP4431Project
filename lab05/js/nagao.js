051b41da947f06b296a66f65f5f19612f0129008
shishutongsst <sshiah@connect.ust.hk>
2022-05-08 17:42:08 +0800

    nagao
    shishutong

--- lab05/js/nagao.js
+++ lab05/js/nagao.js
@@ -82,18 +82,24 @@
         for (var y = 0; y < inputData.height; y++) {
             for (var x = 0; x < inputData.width; x++) {
                 // Find the statistics of the four sub-regions
-                var regionA = regionCorStat(x - 1, y - 1, 1, -1, -1, 1, inputData);
-                var regionB = regionCorStat(x + 1, y - 1, -1, -1, 1, 1, inputData);
-                var regionC = regionCorStat(x - 1, y + 1, -1, -1, 1, 1, inputData);
-                var regionD = regionCorStat(x + 1, y + 1, 1, -1, -1, 1, inputData);
-				var regionE = regionCorStat(x - 1, y    , 1, -1,  1, 1, inputData);
-				var regionF = regionCorStat(x - 1, y    , 1, -1,  1, 1, inputData);
-				var regionE = regionCorStat(x - 1, y    , 1, -1,  1, 1, inputData);
+                var regionA = regionCorStat(x - 1, y - 1, 1, -1, -1,  1, inputData);
+                var regionB = regionCorStat(x + 1, y - 1, -1, -1, 1,  1, inputData);
+                var regionC = regionCorStat(x - 1, y + 1, -1, -1, 1,  1, inputData);
+                var regionD = regionCorStat(x + 1, y + 1, 1, -1, -1,  1, inputData);
+				var regionE = regionCorStat(x - 1, y    , 1, -1,  1,  1, inputData);
+				var regionF = regionCorStat(x    , y - 1, -1, 1,  1,  1, inputData);
+				var regionG = regionCorStat(x    , y + 1, -1, -1, 1, -1, inputData);
+				var regionH = regionCorStat(x + 1, y    , -1, -1,-1,  1, inputData);
+				
+				var regionI = regionCorStat(x    , y    , -2, -2,-2,  2, inputData);
 				
 
                 // Get the minimum variance value
                 var minV = Math.min(regionA.variance, regionB.variance,
-                                    regionC.variance, regionD.variance);
+                                    regionC.variance, regionD.variance,
+									regionE.variance, regionF.variance,
+									regionG.variance, regionH.variance,
+									regionI.variance);
 
                 var i = (x + y * inputData.width) * 4;
 
@@ -119,7 +125,33 @@
                     outputData.data[i]     = regionD.mean.r;
                     outputData.data[i + 1] = regionD.mean.g;
                     outputData.data[i + 2] = regionD.mean.b;
-                }
+					break;
+				case regionE.variance:
+                    outputData.data[i]     = regionE.mean.r;
+                    outputData.data[i + 1] = regionE.mean.g;
+                    outputData.data[i + 2] = regionE.mean.b;
+                    break;
+                case regionF.variance:
+                    outputData.data[i]     = regionF.mean.r;
+                    outputData.data[i + 1] = regionF.mean.g;
+                    outputData.data[i + 2] = regionF.mean.b;
+                    break;
+                case regionG.variance:
+                    outputData.data[i]     = regionG.mean.r;
+                    outputData.data[i + 1] = regionG.mean.g;
+                    outputData.data[i + 2] = regionG.mean.b;
+                    break;
+                case regionH.variance:
+                    outputData.data[i]     = regionH.mean.r;
+                    outputData.data[i + 1] = regionH.mean.g;
+                    outputData.data[i + 2] = regionH.mean.b;
+                    break;
+                case regionI.variance:
+                    outputData.data[i]     = regionI.mean.r;
+                    outputData.data[i + 1] = regionI.mean.g;
+                    outputData.data[i + 2] = regionI.mean.b;
+                    break;					
+               }
             }
         }
     }
