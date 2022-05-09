bbdb99bbc8981556b5bcf0cca0ae52b017538007
CaiXingLIAO <cliaoaa@connect.ust.hk>
2022-05-09 23:17:51 +0800

    tomita

--- lab05/js/tomita.js
+++ lab05/js/tomita.js
@@ -4,31 +4,21 @@
     /*
      * Apply Tomita filter to the input data
      */
-    imageproc.tomita = function(inputData, outputData, size) {
+    imageproc.tomita = function(inputData, outputData, size) { //size may be 5*5 or 9*9 or 13*13
         console.log("Applying Tomita filter...");
-
-        /*
-         * TODO: You need to extend the tomita function to include different
-         * sizes of the filter
-         *
-         * You need to clearly understand the following code to make
-         * appropriate changes
-         */
 
         /*
          * An internal function to find the regional stat centred at (x, y)
          */
-		
-		var shiftSize = (size-1)/4;
-		var sideLength = (size-1)/2 + 1;
-		var regionSize = sideLength * sideLength;
-		 
+        var subSize = (size + 1)/2;
+        var diff = (size - subSize)/2;
+
         function regionStat(x, y) {
             // Find the mean colour and brightness
             var meanR = 0, meanG = 0, meanB = 0;
             var meanValue = 0;
-            for (var j = -shiftSize; j <= shiftSize; j++) {
-                for (var i = -shiftSize; i <= shiftSize; i++) {
+            for (var j = - (subSize-1)/2; j <= (subSize-1)/2; j++) {
+                for (var i = - (subSize-1)/2; i <= (subSize-1)/2; i++) {
                     var pixel = imageproc.getPixel(inputData, x + i, y + j);
 
                     // For the mean colour
@@ -40,22 +30,22 @@
                     meanValue += (pixel.r + pixel.g + pixel.b) / 3;
                 }
             }
-            meanR /= regionSize;
-            meanG /= regionSize;
-            meanB /= regionSize;
-            meanValue /= regionSize;
+            meanR /= (subSize * subSize);
+            meanG /= (subSize * subSize);
+            meanB /= (subSize * subSize);
+            meanValue /= (subSize * subSize);
 
             // Find the variance
             var variance = 0;
-            for (var j = -shiftSize; j <= shiftSize; j++) {
-                for (var i = -shiftSize; i <= shiftSize; i++) {
+            for (var j = - (subSize-1)/2; j <= (subSize-1)/2; j++) {
+                for (var i = - (subSize-1)/2; i <= (subSize-1)/2; i++) {
                     var pixel = imageproc.getPixel(inputData, x + i, y + j);
                     var value = (pixel.r + pixel.g + pixel.b) / 3;
 
                     variance += Math.pow(value - meanValue, 2);
                 }
             }
-            variance /= regionSize;
+            variance /= (subSize * subSize);
 
             // Return the mean and variance as an object
             return {
@@ -67,14 +57,15 @@
         for (var y = 0; y < inputData.height; y++) {
             for (var x = 0; x < inputData.width; x++) {
                 // Find the statistics of the four sub-regions
-                var regionA = regionStat(x - shiftSize, y - shiftSize, inputData);
-                var regionB = regionStat(x + shiftSize, y - shiftSize, inputData);
-                var regionC = regionStat(x - shiftSize, y + shiftSize, inputData);
-                var regionD = regionStat(x + shiftSize, y + shiftSize, inputData);
+                var regionA = regionStat(x - diff, y - diff, inputData);
+                var regionB = regionStat(x + diff, y - diff, inputData);
+                var regionC = regionStat(x - diff, y + diff, inputData);
+                var regionD = regionStat(x + diff, y + diff, inputData);
+                var regionCenter = regionStat(x, y, inputData);
 
                 // Get the minimum variance value
                 var minV = Math.min(regionA.variance, regionB.variance,
-                                    regionC.variance, regionD.variance);
+                                    regionC.variance, regionD.variance, regionCenter.variance);
 
                 var i = (x + y * inputData.width) * 4;
 
@@ -100,6 +91,12 @@
                     outputData.data[i]     = regionD.mean.r;
                     outputData.data[i + 1] = regionD.mean.g;
                     outputData.data[i + 2] = regionD.mean.b;
+                    break;
+                case regionCenter.variance:
+                    outputData.data[i]     = regionCenter.mean.r;
+                    outputData.data[i + 1] = regionCenter.mean.g;
+                    outputData.data[i + 2] = regionCenter.mean.b;
+                    break;
                 }
             }
         }
