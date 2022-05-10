e1e566ec8813bbff628d4fabc17ffdc917a7504f
lxb <lxb007981@hotmail.com>
2022-05-10 12:33:25 +0800

    style: format code -S

--- lab05/js/papari.js
+++ lab05/js/papari.js
@@ -1,28 +1,20 @@
-(function(imageproc) {
+(function (imageproc) {
     "use strict";
 
     /*
      * Apply Tomita filter to the input data
      */
-    imageproc.papari = function(inputData, outputData, q, N, sigma,filterSize, isColor) {
+    imageproc.papari = function (inputData, outputData, q, N, sigma, filterSize, isColor) {
         console.log("Applying Papari filter...");
-
-        /*
-         * TODO: You need to extend the tomita function to include different
-         * sizes of the filter
-         *
-         * You need to clearly understand the following code to make
-         * appropriate changes
-         */
 
         /*
          * An internal function to find the regional stat centred at (x, y)
          */
-		var size = q;
-		var shiftSize = (size-1)/4;
-		var sideLength = (size-1)/2 + 1;
-		var regionSize = sideLength * sideLength;
-		 
+        var size = q;
+        var shiftSize = (size - 1) / 4;
+        var sideLength = (size - 1) / 2 + 1;
+        var regionSize = sideLength * sideLength;
+        
         function regionStat(x, y) {
             // Find the mean colour and brightness
             var meanR = 0, meanG = 0, meanB = 0;
@@ -59,7 +51,7 @@
 
             // Return the mean and variance as an object
             return {
-                mean: {r: meanR, g: meanG, b: meanB},
+                mean: { r: meanR, g: meanG, b: meanB },
                 variance: variance
             };
         }
@@ -74,35 +66,35 @@
 
                 // Get the minimum variance value
                 var minV = Math.min(regionA.variance, regionB.variance,
-                                    regionC.variance, regionD.variance);
+                    regionC.variance, regionD.variance);
 
                 var i = (x + y * inputData.width) * 4;
 
                 // Put the mean colour of the region with the minimum
                 // variance in the pixel
                 switch (minV) {
-                case regionA.variance:
-                    outputData.data[i]     = regionA.mean.r;
-                    outputData.data[i + 1] = regionA.mean.g;
-                    outputData.data[i + 2] = regionA.mean.b;
-                    break;
-                case regionB.variance:
-                    outputData.data[i]     = regionB.mean.r;
-                    outputData.data[i + 1] = regionB.mean.g;
-                    outputData.data[i + 2] = regionB.mean.b;
-                    break;
-                case regionC.variance:
-                    outputData.data[i]     = regionC.mean.r;
-                    outputData.data[i + 1] = regionC.mean.g;
-                    outputData.data[i + 2] = regionC.mean.b;
-                    break;
-                case regionD.variance:
-                    outputData.data[i]     = regionD.mean.r;
-                    outputData.data[i + 1] = regionD.mean.g;
-                    outputData.data[i + 2] = regionD.mean.b;
+                    case regionA.variance:
+                        outputData.data[i] = regionA.mean.r;
+                        outputData.data[i + 1] = regionA.mean.g;
+                        outputData.data[i + 2] = regionA.mean.b;
+                        break;
+                    case regionB.variance:
+                        outputData.data[i] = regionB.mean.r;
+                        outputData.data[i + 1] = regionB.mean.g;
+                        outputData.data[i + 2] = regionB.mean.b;
+                        break;
+                    case regionC.variance:
+                        outputData.data[i] = regionC.mean.r;
+                        outputData.data[i + 1] = regionC.mean.g;
+                        outputData.data[i + 2] = regionC.mean.b;
+                        break;
+                    case regionD.variance:
+                        outputData.data[i] = regionD.mean.r;
+                        outputData.data[i + 1] = regionD.mean.g;
+                        outputData.data[i + 2] = regionD.mean.b;
                 }
             }
         }
     }
- 
+
 }(window.imageproc = window.imageproc || {}));
